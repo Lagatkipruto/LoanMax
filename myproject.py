cdb8b387d8635e6182f13224fe400854fc85b836
emmylem <benjaminosondu55@gmail.com>
2023-07-03 11:01:06 +0000

    login route update for LoanMax

--- myproject.py
+++ myproject.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('loan.html')
+
+@app.route('/login', methods=["POST"])
+def login():
+    return render_template('login.html')
 
 
 if __name__ == "__main__":
